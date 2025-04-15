65186cd83705198a3549fadef3476444f40f8b03
shetty49 <95335320+shetty49@users.noreply.github.com>
2025-04-15 16:12:47 +0530

    Create evaluate.py

---
+++ app/evaluate.py
@@ -0,0 +1,9 @@
+from model_func import preprocess_split, preprocess_scale, model_load, evaluate
+
+def test_performace() -> None:
+    model = model_load("model/model.pkl")
+    X_train, X_test, y_train, y_test = preprocess_split("model/data/elementary.csv")
+    X_train = preprocess_scale(X_train)
+    X_test = preprocess_scale(X_test)
+    r2, mse = evaluate(model, X_test, y_test)
+    assert r2 > 0.5
